f630c175c277eb97568bf360cea27182161ef99b
Jim Brewer <jebrewer@stanford.edu>
2015-11-07 22:26:02 -0800

    Jim Brewer Initial commit of segment function. Segmentation done based on convex hulls and bounding box.

---
+++ fn_segment.m
@@ -0,0 +1,137 @@
+function [ characters ] = fn_segment(eq, showFigs, figNum)
+%fn_segment Segment and return characters from binary image
+%
+%   eq - A binary image [0,1] with desired characters in black, bkgrd white.
+%   showFigs - Boolean. If true will show figures with processed output.
+%       This includes the inverted binarized image with centroids, convex
+%       hull, and bounding box. And figure showing each extracted character
+%   figNum - The figure(#) to start at for displayed figures.
+%   Returns: A struct containing a matrix of each extracted character and
+%   the x/y location of the centroid of that character in the original img.
+%
+%   Detailed Description: This function will extract individual
+%   characters/obects from the passed binary image. It assumes each
+%   character has a stroke width of atleast 2-3 pixels and there is a separation
+%   between each character of at least 1 pixel. Interioir contours and
+%   regions are ignored (e.g. the loops in a B are ignored so it is 1
+%   region).
+%   Currently it doesn't the edges of the character with the convex hull in
+%   case any characters overlap neigboring bounding boxes. In initial test
+%   images this isn't necessary, but the code is in here, just commented
+%   out to perform this.
+%   This function simply creates an edge map of the characters (same size
+%   as original) and then finds the centroid, convex hull, and bounding
+%   boxes which are then used for extraction.
+
+if nargin == 1
+    showFigs = false;
+    figNum = 1;
+elseif nargin == 2
+    figNum = 1;
+end
+
+% Invert image
+eq_inv = ones(size(eq)) - eq;
+
+% Create Edge Map
+se = ones(3,3);
+exp = imerode(eq_inv, se);
+eq_edges = xor(exp, eq_inv);
+
+% Find Centroid for x/y location of each character
+s = regionprops(eq_edges, 'Centroid');
+ch = regionprops(eq_edges, 'ConvexHull');
+bb = regionprops(eq_edges, 'BoundingBox');
+% Matrix of centroid locations
+loc = cat(1, s.Centroid);
+% Matrix of boundingbox corner and sizes
+boundingboxes = cat(1, bb.BoundingBox);
+
+% Check if entire ConvexHull is inside another Convex Hull
+%  What to do with partially in? Ignore for now.
+idx = [];
+for i = 1:size(loc, 1)
+    poly = cat(1, ch(i).ConvexHull);
+    x = poly(:,1)';
+    y = poly(:,2)';
+    for j = 1:size(ch,1)
+        poly = cat(1, ch(j).ConvexHull);
+        x_ch = poly(:,1)';
+        y_ch = poly(:,2)';
+        if((sum(~inpolygon(x,y,x_ch,y_ch)) == 0) && i ~= j)
+            % Mark index to remove
+            idx = [idx i];
+        end
+    end
+end
+% Remove duplicates
+idx = unique(idx);
+% Remove found interior points
+if(~isempty(idx))
+    for i = size(idx,2):-1:1
+        loc(idx(i),:)=[];
+        ch(idx(i))=[];
+        boundingboxes(idx(i),:)=[];
+    end
+end
+
+% Round to whole pixel values and ensure still contains character for
+%   character extraction purposes
+boundingboxes = floor(boundingboxes);
+boundingboxes(:,3:4) = boundingboxes(:,3:4) + 1;
+
+% Init struct to contain extracted characters and their x/y locations
+characters(size(loc,1)).centroid = [];
+
+% Extract each character from binary image based on bounding boxes
+%   If there are issues with overlapping characters into nearby bounding
+%   boxes, can use the Convex Hulls to create masks around desired char
+for i = 1 : size(loc,1)
+    characters(i).centroid = loc(i,:);
+    
+    % Create and apply Convex Hull mask before extraction
+    % To use, uncomment and comment out/delete the repeated
+    % characters(i)... line below
+%     x_ch = ch(i).ConvexHull(:,1);
+%     y_ch = ch(i).ConvexHull(:,2);
+%     eq_mask = ones(size(eq)) - eq;
+%     eq_mask = eq_mask .* poly2mask(x_ch, y_ch, size(eq,1), size(eq,2));
+%     eq_mask = ones(size(eq)) - eq_mask;
+%     characters(i).char = eq_mask(boundingboxes(i,2):boundingboxes(i,2)+boundingboxes(i,4),...
+%         boundingboxes(i,1):boundingboxes(i,1)+boundingboxes(i,3),:);
+    
+    characters(i).char = eq(boundingboxes(i,2):boundingboxes(i,2)+boundingboxes(i,4),...
+        boundingboxes(i,1):boundingboxes(i,1)+boundingboxes(i,3),:);
+end
+
+%% Only show figures if boolean passed to show them
+if showFigs
+    % Plot centroids, bounding boxes, convex hulls on image
+    figure(figNum);
+    imshow(eq_edges);
+    for i = 1:size(loc, 1) % Assumes same # centroid and convex hull
+        % Plot centroids
+        x = loc(i,1);
+        y = loc(i,2);
+        text(x, y, '*' ,'Color', 'yellow', 'FontSize', 14);
+        % Plot Convex Hull
+        poly = cat(1, ch(i).ConvexHull);
+        x = poly(:,1)';
+        y = poly(:,2)';
+        hold on;
+        plot([x x(1)],[y y(1)],'r-');
+        hold off;
+        % Plot Bounding Box
+        rectangle('position',boundingboxes(i,:),'Edgecolor','g')
+    end
+
+    % Show all extracted characters
+    figure(figNum + 1);
+    for i =1:size(characters,2)
+       subplot(2,ceil(size(characters,2)/2),i);
+       imshow(characters(i).char);
+    end
+end
+
+end
+
