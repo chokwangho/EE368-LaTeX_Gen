47f2fd9332bcd2057f2417ad3e52d5d005e29c8f
Jim Brewer <jebrewer@stanford.edu>
2015-11-16 18:25:36 -0800

    Jim Brewer Inital commit of function to create the identifier vector for a character to base matching on. This contains the Central MOment of Intertia, circular topology and circular arc ratios.

---
+++ fn_createIdent.m
@@ -0,0 +1,139 @@
+function [ identifier ] = fn_createIdent(char, printIdent, showFigs)
+%createIdent - Create the identifier vector with normalized central moment
+% of inertia, the number of chracter crossings for each concentric circle,
+% and the 2 largest arc length ratio for k-1 largest concentric circles.
+%   
+%   printIdent = Prints out cetnral moment, counts and ratio identifier
+%   showFigs - Set to true to show extracted circles and figure with
+%   circles overlaid.
+%
+%   "coding" is a struct that contains the extracted vectors, counts, and 
+%       and rations but is not currently returned.
+%   
+%   Assumes: Character is binary image (character = 0) tightly cropped.
+
+if nargin == 1
+    printIdent = false;
+    showFigs = false;
+elseif nargin == 2
+    showFigs = false;
+end
+% Parameterss
+k = 8;
+identifier = zeros(1,2*k);
+
+% Total number of pixels in character
+N = sum(~char(:));
+% Find x,y positions of 0 elements (character)
+[y, x] = find(~char);
+
+% Find centroid
+cent = regionprops(char,'centroid');
+cent = cat(1, cent.Centroid);
+cent_x = cent(1);
+cent_y = cent(2);
+
+% Find central moment of inertia
+I = sum((x - cent_x).^2 + (y - cent_y).^2);
+IN2 = I / N^2;
+identifier(1) = IN2;
+if(printIdent)
+   fprintf('Normalized Central Moment: %.4f\n', IN2); 
+end
+
+% Extract Circular Topology Template and Diameter Ratio
+y = size(char,1);
+x = size(char,2);
+dr = max(x - cent_x, y - cent_y) / k;
+[c, r] = meshgrid(1:x, 1:y);
+
+% Display Extracted circles
+if(showFigs)
+    figure(1);
+    % Create copy of character to display circles overlaid
+    tempcirc = char;
+end
+
+% Init coding vector for storing extra information
+coding(k).circVec=[];
+for i = 1:k
+    rad = dr * i;
+    % Create circle line of logical 1s to extract template
+    C = xor(sqrt((c-cent_x).^2+(r-cent_y).^2)<=rad, ...
+        sqrt((c-cent_x).^2+(r-cent_y).^2)<=(rad-1));
+    % Extract circular template linear indices (do top and bottom separate 
+    % because of how linear indices work)
+    C1 = C(1:round(cent_y),:);
+    C2 = C(round(cent_y)+1:end,:);
+    idx1 = find(C1);
+    idx2 = find(C2);
+    temp1 = char(1:round(cent_y),:);
+    temp2 = char(round(cent_y)+1:end,:);
+    % Extract vector of elements on circle. Start at180deg, CW
+    circVec = [temp1(idx1) ; flipud(temp2(idx2))];
+    
+    % Show circles overlaid on character
+    if(showFigs)
+        idx = find(C);
+        tempcirc(idx) = .5;
+    end
+    
+    % Extract values from character based on circular indices
+    
+    % Need to do closing of gaps? Initially seems better without
+%     circVec = ones(size(circVec))-imclose(ones(size(circVec))-circVec,ones(2,1));
+    
+    % Save vector for debugging or future use
+    coding(i).circ = circVec;
+    
+    % Count number of sections of at least 2 0s. This is the number of
+    % character sections the circle goes through.
+    cnt = strfind(circVec',[0 0]);
+    coding(i).count = length(cnt(diff([1 cnt])~=1));
+    identifier(1+i) = coding(i).count;
+    % Handle wrap around of vector since it is a circle
+    if(circVec(1) == 0 && circVec(end) == 0)
+        coding(i).count = coding(i).count - 1;
+    end
+    subplot(2,4,i);
+    imshow(coding(i).circ);
+    str = sprintf('%d',coding(i).count);
+    title(str);
+    
+    % Find 2 longest arcs of background and divide diff by total length
+    % If 1s (background) wrap around, move all 1s to one side.
+    circ = length(circVec);
+    if(circVec(1) == 1 && circVec(end) == 1)
+        idx = find(circVec==0,1,'last');
+        circVec = [circVec(idx+1:end); circVec(1:idx)];
+    end
+    % Vector of length of each section of 1s
+    B = [0 circVec' 0]; % Pad with 0s for diff
+    bgrd_len = find(diff(B)==-1) - find(diff(B)==1);
+    % If no crossings, set ratio to 0
+    if(coding(i).count < 1);
+        d2 = 0;
+        d1 = 0;
+    else
+        % Get two longest sections (arcs)
+        [d2, idx_d2] = max(bgrd_len);
+        bgrd_len(idx_d2)=NaN;
+        d1 = max(bgrd_len);
+    end
+    % Gind ratio of difference of two longest arcs by circumference
+    coding(i).ratio = (d2-d1) / circ;
+    if(i > 1) % Only keep k-1 largest ratios
+        identifier(k+i) = coding(i).ratio;
+    end
+    fprintf('Count=%d, d2=%d, d1=%d, c=%d, ratio = %.4f\n',...
+        coding(i).count,d2,d1,circ,coding(i).ratio);
+end
+
+% Show character with circles overlaid
+if(showFigs)
+    figure(2);
+    imshow(tempcirc);
+end
+
+end
+
