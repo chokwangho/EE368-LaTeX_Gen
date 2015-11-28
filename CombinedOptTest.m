a505473875dd63c47448e71a8d9bf9a0ac041cec
James Sun <james.b.sun@gmail.com>
2015-11-27 23:20:46 -0600

    Test Results

---
+++ CombinedOptTest.m
@@ -0,0 +1,15 @@
+lighting_dir = 'Lighting\';
+combined_dir = 'Opt\';
+results_dir = [combined_dir 'Results\'];
+listing = dir(lighting_dir);
+
+for i = 1:length(listing)
+    fname = listing(i).name;
+    if ~listing(i).isdir && ~strcmp(fname,'.') && ~strcmp(fname,'..')
+        img = imread([lighting_dir fname]);
+        bw_img = fn_lighting_compensation(img);
+        detected_angle = fn_deskew(bw_img)
+        out_img = imrotate(bw_img,-detected_angle);
+        imwrite(out_img,[results_dir 'result_' fname]);
+    end
+end
