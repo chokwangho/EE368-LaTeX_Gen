8c9f0d8d8c81a179dd7d0c9a204ce277c6a33886
James Sun <james.b.sun@gmail.com>
2015-11-28 11:33:43 -0600

    Additional tests

--- SkewTest.m
+++ SkewTest.m
@@ -1,23 +1,26 @@
 
 
-img = rgb2gray(imread('LaTeX Equations\eq8.jpg'));
+img = rgb2gray(imread('LaTeX Equations\eq1_hr.jpg'));
 
 detected = zeros(size(-5:0.5:15));
 i = 1;
 err = detected;
-for theta = -5:0.5:15
-rotated = imrotate(img,theta,'bilinear');
-Mrot = ~imrotate(true(size(img)),theta,'bilinear');
-rotated(Mrot&~imclearborder(Mrot)) = 255;
-thresh = graythresh(rotated);
-bw_skew = im2bw(rotated, thresh);
 
-detected(i) = fn_deskew2(bw_skew);
-if theta ==0
-    err(i) = abs((detected(i)-theta));
-else
-    err(i) = abs((detected(i)-theta)/theta);
-end
-i = i+1;
+for theta = -5:0.5:15
+    rotated = imrotate(img,theta);
+    Mrot = ~imrotate(true(size(img)),theta,'bilinear');
+    rotated(Mrot&~imclearborder(Mrot)) = 255;
+    thresh = graythresh(rotated);
+    bw_skew = im2bw(rotated, thresh);
+    
+    tic
+    detected(i) = fn_deskew2(bw_skew);
+    toc
+    if theta ==0
+        err(i) = abs((detected(i)-theta));
+    else
+        err(i) = abs((detected(i)-theta)/theta);
+    end
+    i = i+1;
 end
 
--- fn_deskew2.m
+++ fn_deskew2.m
@@ -1,9 +1,16 @@
 function [ deskew_img ] = fn_deskew2( bw_img )
+
+% pre_theta = 10;
+% rotated = imrotate(bw_img,pre_theta);
+% Mrot = ~imrotate(true(size(bw_img)),pre_theta);
+% rotated(Mrot&~imclearborder(Mrot)) = 255;
+
 edges = edge(bw_img,'canny');
 [H,T,~] = hough(edges,'RhoResolution',1,'ThetaResolution', 0.1 );
 
 P = houghpeaks(H,1);
 
-detected_angle = T(P(1,2))-90;
-deskew_img = detected_angle;
+deskewing_angle = T(P(1,2))-90; 
+% deskewing_angle = deskewing_angle+pre_theta;
+deskew_img = deskewing_angle;
 end
