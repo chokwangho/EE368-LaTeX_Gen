13b2df15000c482539923be60c6541086639c609
jsun2013 <james.b.sun@gmail.com>
2015-12-01 01:46:36 -0800

    Updated soften_edges to use gaussian blur

--- fn_soften_edges.m
+++ fn_soften_edges.m
@@ -1,14 +1,13 @@
 function softened = fn_soften_edges(bw_img, se_size)
-%FN_SOFTEN_EDGES Softens the edges of a binary image using an opening
-%operation
+%FN_SOFTEN_EDGES Softens the edges of a binary image using a Gaussian Blur.
+% This function expects the image to have text in the background (black
+% text on white)
     
 if nargin == 1
-    se_size = 5;
+    se_size = 7;
 end
 
-se = true(se_size,se_size);
-se = strel('disk',se_size);
-
-softened = imopen(bw_img,se);
+fg = fspecial('gaussian', se_size, 3);
+softened = ~imfilter(~bw_img, fg, 'conv','replicate');
 
 end
