130a59b5c536eb86301b758a663ab0d840e41502
James Sun <james.b.sun@gmail.com>
2015-12-01 22:17:12 -0800

    Updated Batch File for Demo PC paths

--- Demo/demo_latex_gen.m
+++ Demo/demo_latex_gen.m
@@ -3,22 +3,24 @@ close all;
 
 my_full_path = which('demo_latex_gen');
 [file_dir,~,~] = fileparts(my_full_path);
-cd('E:\Github\EE368-LaTeX_Gen\Demo\');
+cd('C:\Users\James\Documents\GitHub\EE368-LaTeX_Gen\Demo\');
+
+box_photo_dir = 'C:\Users\James\Box Sync\Project\DemoPictures';
 
 addpath('..');
-listing = dir(pwd);
+listing = dir(box_photo_dir);
 
 for i = 1:length(listing)
     fname = listing(i).name;
     [~,~,ext] = fileparts(fname);
     if strcmp(ext,'.jpg')
-        [eq_string, output_file] = fn_demo(fullfile(pwd,fname),false);
+        [eq_string, output_file] = fn_demo(fullfile(box_photo_dir,fname),false);
         disp(eq_string);
         system(['pdflatex ' output_file]);
         system([output_file '.pdf']);
         system(['write ' output_file '.tex']);
         
-        movefile(fname,['Finished\' fname]);
+        movefile(fullfile(box_photo_dir,fname) ,fullfile(box_photo_dir,'Finished',fname));
         delete('*.aux');
         delete('*.log');
         
