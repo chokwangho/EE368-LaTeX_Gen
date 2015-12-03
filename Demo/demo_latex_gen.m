f616c462001716ebc2efba75b7766f1376e3e4fd
James Sun <james.b.sun@gmail.com>
2015-12-02 21:10:21 -0800

    Updated demo script

--- Demo/demo_latex_gen.m
+++ Demo/demo_latex_gen.m
@@ -14,16 +14,23 @@ for i = 1:length(listing)
     fname = listing(i).name;
     [~,~,ext] = fileparts(fname);
     if strcmp(ext,'.jpg')
-        [eq_string, output_file] = fn_demo(fullfile(box_photo_dir,fname),false);
+        [eq_string, output_file] = fn_demo(fullfile(box_photo_dir,fname),true);
         disp(eq_string);
         system(['pdflatex ' output_file]);
-        system([output_file '.pdf']);
+        winopen([output_file '.pdf']);
         system(['write ' output_file '.tex']);
         
         movefile(fullfile(box_photo_dir,fname) ,fullfile(box_photo_dir,'Finished',fname));
         delete('*.aux');
         delete('*.log');
-        
+		if i+1 <= length(listing)
+			h = helpdlg(['Continue to ' listing(i+1).name '?'], 'Continue?');
+			waitfor(h);
+		end
 
     end
 end
+
+
+h = helpdlg(['Ready to exit?'], 'Exit?');
+waitfor(h);
--- Demo/mat.bat
+++ Demo/mat.bat
@@ -1,1 +1,1 @@
-matlab -nosplash -nodesktop -minimize -r "run('C:\Users\James\Documents\Github\EE368-LaTeX_Gen\Demo\demo_latex_gen.m')"
+matlab -nosplash -nodesktop -minimize -r "run('C:\Users\James\Documents\Github\EE368-LaTeX_Gen\Demo\demo_latex_gen.m');quit"; 
